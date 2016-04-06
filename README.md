47f6e18738b25e35632e370f42c933d89782f8c6
Khairul <kyu.helf@gmail.com>
2016-04-06 09:29:47 +0700

    Accept file as input test and write result to CSV

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 ## Data Source
 https://data.kpu.go.id/dps.php
 
-## Run
-> python name.py
+### Run
+```python name.py test_file.input result.csv```
+
+
+### Input File
+
+Multiple name, seperate by new line
+
+```
+Devita Pearce
+Muhammad Hatta
+Ridwan Kamil
+```
+
+
+### Result file
+
+Result will be write in csv
+
+| Tested_Name | Predicted_Gender | Female_Score | Male_Score | Percentage |
+| --- | --- | --- | --- | --- |
+| 'Devita Pearce' | f | 35 | 0 | 1.0 |
+| 'Muhammad Hatta' | m | 11 | 101 | 0.901785714286 |
+| 'Ridwan Kamil' | m | 14 | 73 | 0.83908045977 |
--- name.py
+++ name.py
@@ -2,14 +2,15 @@ import string
 from nltk.util import ngrams
 from sys import argv
 
-test_name = argv[1]
+input_file = argv[1]
+output_file = argv[2]
 
 raw = open('names.pair', 'r').read().split('\n')
 raw = filter(string.strip, raw)
 
 def mapper(pair):
   name, gender = pair.strip().split('@')
-  return [name.strip(), gender.strip()]
+  return [name.strip().lower(), gender.strip().lower()]
 
 names = map(mapper, raw)
 
@@ -17,35 +18,51 @@ bi = dict()
 
 for pair in names:
   name, gender = pair
-  two_grams = ngrams(name, 3)
-  two_grams = list(two_grams)
-  for two in two_grams[:-1]:
-    bi.setdefault(two, dict(m=0, f=0))
-    bi[two][gender] += 1
-  bi.setdefault(two_grams[-1], dict(m=0, f=0))
-  bi[two_grams[-1]][gender] += 2
+  tri_grams = ngrams(name, 3)
+  tri_grams = list(tri_grams)
+
+  if len(tri_grams) > 0:
+    for two in tri_grams[:-1]:
+      bi.setdefault(two, dict(m=0, f=0))
+      bi[two][gender] += 1
+    bi.setdefault(tri_grams[-1], dict(m=0, f=0))
+    bi[tri_grams[-1]][gender] += 2
 
 
+out_file = open(output_file, 'w')
+out_file.write('Tested_Name, Predicted_Gender, Female_Score, Male_Score, Percentage\n')
 
-# Test
-t_bi = bi
-m_total = 0
-f_total = 0
+test_names = open(input_file, 'r').read().split('\n')
+test_names = filter(string.strip, test_names)
 
-t_names = map(string.strip, test_name.split(' '))
-for t_name in t_names:
-  two_grams = ngrams(t_name, 3)
-  for two in two_grams:
-    t_bi.setdefault(two, dict(m=0, f=0))
-    m_total += t_bi[two]['m']
-    f_total += t_bi[two]['f']
+for test_name in test_names:
+  t_names = map(string.strip, test_name.split())
+  t_names = map(string.lower, t_names)
 
-print test_name
+  t_bi = bi
+  m_total = 0
+  f_total = 0
+  t_gender = '?'
+  percentage = 0.0
+  all_total = 0
 
-if f_total > m_total:
-  print 'female'
-else:
-  print 'male'
+  for t_name in t_names:
+    two_grams = ngrams(t_name, 3)
+    for two in two_grams:
+      t_bi.setdefault(two, dict(m=0, f=0))
+      m_total += t_bi[two]['m']
+      f_total += t_bi[two]['f']
 
-print f_total, m_total
+  all_total = float(f_total + m_total)
 
+  if f_total > m_total:
+    t_gender = 'f'
+    percentage = f_total / all_total
+
+  elif m_total > f_total:
+    t_gender = 'm'
+    percentage = m_total / all_total
+
+  out_file.write("'" + test_name + "', " + t_gender + ", " + str(f_total) + ", " + str(m_total) + ", " + str(percentage) + "\n")
+
+out_file.close()
